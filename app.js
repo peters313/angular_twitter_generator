9a1f4600b0c0bf32a51ee1254676421b5cfaa34e
peters313 <ericpeters@Erics-MacBook-Pro.local>
2015-08-18 17:10:10 -0500

    generating 10 random handles

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 var app = angular.module('myApp', []);
 
-app.controller('MyController', function($scope, AdjectiveService, NounService){
-    $scope.adj = AdjectiveService.adj;
-    $scope.noun = NounService.noun;
+app.controller('MyController', function($scope, CompileService){
+    $scope.twitter = CompileService;
 });
 
 app.factory('AdjectiveService', function(){
-   var adjArray = ['adj1', 'adj2', 'adj3', 'adj4', 'adj5', 'adj6', 'adj7', 'adj8', 'adj9', 'adj10'];
-    function randomNumber(min, max){
-        return Math.round(Math.random() * (max - min) + min);
-    }
-    function getRandomAdj(array){
-        var randomAdj = adjArray[randomNumber(0, adjArray.length - 1)];
-        return randomAdj;
-    }
-    var adj = {adj: getRandomAdj(adjArray)};
-    return adj;
+   var adjArray = ['hushed', 'lopsided', 'tangy', 'lowly', 'bloody', 'nonchalant', 'condemned', 'ratty', 'gaping', 'trashy', 'irritating', 'lewd', 'woozy', 'psychedelic', 'grouchy', 'glossy', 'clammy', 'teeny', 'dazzling'];
+    //function randomNumber(min, max){
+    //    return Math.round(Math.random() * (max - min) + min);
+    //}
+    //function getRandomAdj(array){
+    //    var randomAdj = adjArray[randomNumber(0, adjArray.length - 1)];
+    //    return randomAdj;
+    //}
+    //var adj = {adj: getRandomAdj(adjArray)};
+    return adjArray;
 });
 
 app.factory('NounService', function(){
-   var nounArray = ['noun1', 'noun2', 'noun3', 'noun4', 'noun5', 'noun6', 'noun7', 'noun8', 'noun9', 'noun10']
+   var nounArray = ['cherries', 'cattle', 'stitch', 'minute', 'sink', 'chicken', 'fang', 'growth', 'apparatus', 'blow', 'flesh', 'earth', 'rabbits', 'hot', 'taste', 'bottle', 'mice', 'poison', 'veil', 'crayon', 'tongue'];
+    //function randomNumber(min, max){
+    //    return Math.round(Math.random() * (max - min) + min);
+    //}
+    //function getRandomNoun(array){
+    //    var randomNoun = nounArray[randomNumber(0, nounArray.length - 1)];
+    //    return randomNoun;
+    //}
+    //var noun = {noun: getRandomNoun(nounArray)};
+    return nounArray;
+});
+
+app.factory('CompileService', function(NounService, AdjectiveService){
+    var compiledArray = [];
     function randomNumber(min, max){
         return Math.round(Math.random() * (max - min) + min);
     }
-    function getRandomNoun(array){
-        var randomNoun = nounArray[randomNumber(0, nounArray.length - 1)];
-        return randomNoun;
-    }
-    var noun = {noun: getRandomNoun(nounArray)};
-    return noun;
+    for(var i = 0; i<=10; i++) {
+        compiledArray.push(AdjectiveService[randomNumber(0, AdjectiveService.length - 1)] + "_" + NounService[randomNumber(0, NounService.length - 1)]);
+    };
+    return compiledArray;
+    console.log(compiledArray);
 });
+
